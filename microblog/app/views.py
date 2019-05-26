e917a7c0d5e01be3d2b62fb0b2281e4f7e6a3fc1
guleroman <remembername@protonmail.com>
2019-05-26 09:11:00 +0300

    init

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -156,4 +156,4 @@ def dash_overview_2():
 
 if __name__ == '__main__':
 
-    app.run(debug=False,threaded = True, host='0.0.0.0', port=8080)
+    app.run(debug=False,threaded = True, host='0.0.0.0', port=80)
